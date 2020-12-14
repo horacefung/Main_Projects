431ccd180e4b6fec6805afc64a67d5cc168fd53a
horacefung <hf784@stern.nyu.edu>
2020-12-13 22:32:26 -0500

    Update
    - Added IRR calculation for implied ERP

--- Valuation/module/ideas.py
+++ Valuation/module/ideas.py
@@ -13,6 +13,8 @@ import pickle
 from sklearn.externals import joblib
 import warnings
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import scipy
 warnings.filterwarnings('ignore')
 
 
@@ -50,18 +52,41 @@ def rf_country_adj(method, sovereign_bond_orig, sovereign_bond_base, base_bond, 
 	else:
 		return print('Please specify method')
 
-
 	return sovereign_rf
 
 
 
 # --- Equity Risk Premium --- (Some Corporate RP + some country RP) #
-def implied_erp():
-	return None
+def implied_erp(irr, starting_market_price, dividend_buyback_pct, dividend_buyback_growth, stable_growth, period):
+    
+    discount_sum = 0
+    starting_cf = starting_market_price * dividend_buyback_pct
+    cash_flows = [None] * period
+
+    for i in range(0, period):  # Assume CF1 is received
+        cash_flows[i] = starting_cf * ((1 + dividend_buyback_growth) ** (1 + i))
+        
+    # Perpetuity
+    terminal_value = (cash_flows[-1] * (1 + stable_growth)) / ((irr - stable_growth) * (1 + irr) ** period)
+    
+    # NPV sum
+    for i in range(0, len(cash_flows)):
+        discount_sum += cash_flows[i] / (1 + irr)**(1 + i)
+    
+    discount_sum += terminal_value
+    output = discount_sum - starting_market_price
+    
+    return output
+
+
+ root(fun = implied_erp, x0=0.05, args=(1468.36, 0.0402, 0.05, 0.0402, 5))
 
 
 def country_risk_premium(sovereign_rating, default_spread, prs_risk_estimate,
 	sovereign_equity_vol, sovereign_bond_vol):
+
+	'''Choose from 3 options to compute a region's CRP, depending on
+	what the country risk rating is or if its even available'''
 
 	if sovereign_rating == 'AAA':
 		crp = 0
@@ -77,7 +102,8 @@ def country_risk_premium(sovereign_rating, default_spread, prs_risk_estimate,
 
 	return crp
 
-
+# There are other approaches to estimating the CRP coefficients, but
+# a regression is the most robust and data driven method.
 def lambda_regression(returns_df):
 
 	''' Linear regression to estimate lambda coefficients on country risk.
@@ -102,16 +128,27 @@ def lambda_regression(returns_df):
 	# Regression
 	X = returns_df.iloc[:, 1:]
 	y = returns_df.iloc[:, 0]
+
+	regressor = LinearRegression(X, y)
+	y_hat = regressor.predict(X)
+	r2 = r2_score(y, y_hat)
+	adj_r2 = 1-(1-r2)*(len(X)-1)/(len(X)-len(X.columns)-1)
 	
+	# Get country coefficients
+	lambda_df = pd.DataFrame({'premiums' : list(X.columns), 'lambda':regressor.coef_})
 
+	return lambda_df, adj_r2
 
+ 
+def weighted_crp(df):
 
+	'''Take a dataframe of countries/regions, their CRP and the weights.
+	Pretty minor step, to the point where we may not have this as an individual
+	function, but we'll see'''
 
+	df['weighted_crp'] = df['crp'] * df['coefficients']
 
-
-
-
-
+	return df['weighted_crp'].sum()
 
 
 
