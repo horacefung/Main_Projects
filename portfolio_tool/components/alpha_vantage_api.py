3140824dd4661871e80135870b07a5c98852bc72
horacefung <hf784@stern.nyu.edu>
2023-01-15 23:00:16 -0500

    Create cash balance class and update APIs

--- portfolio_tool/components/alpha_vantage_api.py
+++ portfolio_tool/components/alpha_vantage_api.py
@@ -2,14 +2,16 @@
 Alpha Vantage API Wrapper
 '''
 
+import pandas as pd
+from pandas.tseries.offsets import BMonthEnd  
 import requests
-from keys import api_keys
+from keys import secrets
 
-AV_KEY = api_keys['alpha_vantage']
+AV_KEY = secrets['alpha_vantage']
 
 
 # Alpha Vantage API wrappers
-def alpha_vantage_price(ticker, endpoint):
+def security_price(ticker, endpoint):
     ''' API connection to alpha vantage. Allows various price queries, either historical time series
     at different increments or direct quote of current prices.
 
@@ -37,8 +39,46 @@ def alpha_vantage_price(ticker, endpoint):
 
     return data
 
-def alpha_vantage_fred():
+def fred():
     ''' API connection to alpha vantage for FRED economic indicators
     '''
     return None
 
+def fx_rates(curr_from, curr_to, dates=[]):
+    ''' Get monthly exchange rate time series. We can add more features for different frequencies
+    but right now it's not worth it. 
+
+    Keyword Arguments:
+    curr_from -- The current currency
+    curr_ot -- The currency to exchange to
+    '''
+    curr_from = curr_from.upper()
+    curr_to = curr_to.upper()
+    offset = BMonthEnd() 
+
+    if len(dates) == 0:
+        # No specific range of dates, just get current
+        url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={curr_from}&to_currency={curr_to}&apikey={AV_KEY}'
+        r = requests.get(url)
+        data = r.json()
+        rate = float(data['Realtime Currency Exchange Rate']['5. Exchange Rate'])
+        return rate
+    else:
+        # Given specific dates
+        url = f'https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol={curr_from}&to_symbol={curr_to}&apikey={AV_KEY}'
+        r = requests.get(url)
+        data = r.json()
+        data = data['Time Series FX (Monthly)']
+        fx_dates = list(data.keys())
+
+        rates = {}
+        for i in dates:
+            for j in range(1, len(fx_dates)):
+                if (i <= fx_dates[j-1]) & (i >= fx_dates[j]):
+                    # Found a date in between
+                    rates[i] = float(data[fx_dates[j-1]]['4. close'])
+
+        return rates
+
+if __name__ == '__main__':
+    fx_rates('HKD', 'USD', dates=['2021-12-01', '2019-12-30'])
---
+++ portfolio_tool/components/gcp_api.py
@@ -0,0 +1,57 @@
+'''
+Gsheet API Wrapper
+'''
+# Standard imports
+import pandas as pd
+
+# GCP imports
+import gspread
+from google.oauth2 import service_account
+import pandas_gbq   
+
+# Project imports
+from keys import google_credentials, secrets
+
+# Globals
+GC = gspread.service_account_from_dict(google_credentials)
+PROJECT_ID = secrets['project_id']
+CREDENTIALS = service_account.Credentials.from_service_account_info(google_credentials)
+
+# --- Google Sheets Wrapper --- #
+def sheet_to_df(workbook_name, sheet_name, col_range=None):
+    sh = GC.open_by_key(workbook_name)
+    if col_range is None:
+        worksheet = sh.worksheet(sheet_name)
+        list_of_dicts = worksheet.get_all_records()
+        df = pd.DataFrame(list_of_dicts)
+    else:
+        list_of_dicts = sh.values_get(range=sheet_name+f"!{col_range}")['values']
+        df = pd.DataFrame(list_of_dicts[1:])
+        df.columns = list_of_dicts[0]
+    
+    return df
+
+def df_to_sheet(df, workbook_name, sheet_name, col_range):
+    sh = GC.open_by_key(workbook_name)
+    worksheet = sh.worksheet(sheet_name)
+    worksheet.update(col_range, [df.columns.values.tolist()] + df.values.tolist())
+    return print(f"Updated {sheet_name}")
+
+# --- BQ Wrapper --- #
+def bq_to_df(query):
+    df = pandas_gbq.read_gbq(query, project_id=PROJECT_ID)
+    return df
+
+def df_to_bq(df, dataset, table, if_exists='replace'):
+    '''
+    if_exists = fail, replace, append
+    '''
+    pandas_gbq.to_gbq(df, f'{dataset}.{table}', project_id=PROJECT_ID, 
+                    if_exists=if_exists, credentials=CREDENTIALS)
+    return print("Uploaded to BQ table {PROJECT}.{dataset}.{table}")
+
+
+if __name__ == '__main__':
+    #cash = sheet_to_df("Current Cash")
+    df = pd.DataFrame({"my_string": ["a", "b", "c"],"my_int64": [1, 2, 3]})
+    pandas_gbq.to_gbq(df, 'portfolio_tool.test_table', project_id=PROJECT_ID, credentials=CREDENTIALS)
